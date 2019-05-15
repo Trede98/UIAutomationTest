dab27175044cad31cca0304216f9aed4d8c6514a
Fabio Tredesini <fabiotredesini@hotmail.it>
2019-05-15 12:37:27 +0200

    Upload

---
+++ UIAutomationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UIAutomationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UIAutomationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c24051e0-0073-4cc1-8d7a-f1d4d0e29638")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UIAutomationTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace UIAutomationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Application application = Application.Launch(@"C:\Users\Fabio\Desktop\MeteoBot\MeteoChrome.exe");
+            Window window = application.GetWindow("Form1", InitializeOption.NoCache);
+            TextBox textBox = window.Get<TextBox>("textBox1");
+            textBox.SetValue("Vene");
+
+            Button button = window.Get<Button>("button1");
+            button.Click();
+
+            window.Close();
+        }
+    }
+}
